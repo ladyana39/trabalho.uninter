3723e00168f74294452126dd873ff0df81d356b4
ladyana39 <ladyanagames@hotmail.com>
2025-07-06 21:28:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+const botao = document.getElementById("meuBotao");
+  const texto = document.getElementById("meuTexto");
+
+  botao.addEventListener("click", function() {
+    texto.textContent = "Eu sou o caminho, a verdade e a vida. Ninguém vem ao Pai, a não ser por mim! João 14:6.";
+  });
